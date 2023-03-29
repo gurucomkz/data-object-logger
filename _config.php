c86510fcbeda65885036a3090914014050e4c6d9
Sergey Shevchenko <sergey.aegis@gmail.com>
2023-03-29 14:03:04 +1300

    fix: init long storage config properly

--- _config.php
+++ _config.php
@@ -1,1 +1,19 @@
 <?php
+
+use SilverStripe\Core\Environment;
+use SilverStripe\ORM\DB;
+
+function _DataObjectLoggerLongStorageConfig()
+{
+    $dbconfig = [
+        'type' => 'MySQLDatabase',
+        'server' => Environment::getEnv('DATA_OBJECT_LOGGER_LONG_SERVER'),
+        'database' => Environment::getEnv('DATA_OBJECT_LOGGER_LONG_NAME'),
+        'username' => Environment::getEnv('DATA_OBJECT_LOGGER_LONG_USERNAME'),
+        'password' => Environment::getEnv('DATA_OBJECT_LOGGER_LONG_PASSWORD'),
+    ];
+    
+    DB::setConfig($dbconfig, 'longstorage');
+}
+
+_DataObjectLoggerLongStorageConfig();
